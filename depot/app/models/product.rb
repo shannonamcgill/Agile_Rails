d0defe2cd62e3f4a10745f15ee94c2d6cf1092eb
Shannon McGill <shannonamcgill@gmail.com>
2014-01-10 16:50:20 -0600

    fixed product.rb error

--- depot/app/models/product.rb
+++ depot/app/models/product.rb
@@ -7,9 +7,11 @@ validates :image_url, allow_blank: true, format: {
   with: %r{\.(gif|jpg|png)\Z}i,
   message: 'must be a URL for GIF, JPG, or PNG image.'
 }
+    def self.latest
+  Product.order(:updated_at).last
+   end
+
+
 end
 
-def self.latest
-  Product.order(:update_at).last
-end
 
